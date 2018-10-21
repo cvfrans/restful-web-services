21df75a48b2856240987e3f3f756ffd767740c87
frans valdivia <cvfrans@MacBook-Pro-de-frans.local>
2018-10-21 13:49:09 -0500

    my first content

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWordController.java
@@ -0,0 +1,21 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class HelloWordController {
+
+
+	@GetMapping(path="/hello-world")
+	public HelloWorldBean helloWorld() {
+		return new HelloWorldBean("Hello world");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world, %s", name));
+	}
+	
+}
